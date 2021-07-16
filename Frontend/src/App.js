790fe557ecc4fef661045588fcb87d95fd1be883
Seoyeon Kim <2018103585@khu.ac.kr>
2021-07-17 00:26:16 +0900

    change path

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -11,21 +11,30 @@ export const Bdata = React.createContext({
   Blob : new Blob()
 });
 
+export const Burl = React.createContext({
+  Blob : new Blob()
+});
+
 export const Setb = React.createContext(() => {});
+export const Setburl = React.createContext(() => {});
 
 const App = () => {
-  const [blob, setb] = useState();
- // const [url, seturl] = useState();
+  const [blob, setb] = useState(new Blob());
+  const [burl, setburl] = useState();
   return (
     <BrowserRouter>
       <Bdata.Provider value={blob}>
         <Setb.Provider value={setb}>
-          <Route path="/" component={Home} exact={true}/>
-          <Route path="/selection" component={Selection} />
-          <Route path="/result/:model" component={Result} />
-          <Route path="/gallery" component={Gallery} />
-          <Route path="/Preview/:num" component={Preview} />
-          <Route path="/Record/:num" component={Record} />
+          <Burl.Provider value={burl}>
+            <Setburl.Provider value={setburl}>
+              <Route path="/" component={Home} exact={true}/>
+              <Route path="/selection" component={Selection} />
+              <Route path="/result/:model" component={Result} />
+              <Route path="/gallery" component={Gallery} />
+              <Route path="/Preview/:num" component={Preview} />
+              <Route path="/Record/:num" component={Record} />
+            </Setburl.Provider>
+          </Burl.Provider>
         </Setb.Provider>
       </Bdata.Provider>
     </BrowserRouter>
