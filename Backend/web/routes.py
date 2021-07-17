293c891f3f02b7c666c85b09ad6eb00eb5696f67
banggu0321 <99bang@naver.com>
2021-07-17 19:35:15 +0900

    modify routes.py

--- Backend/web/routes.py
+++ Backend/web/routes.py
@@ -74,7 +74,7 @@ def return_result(model_id):
     elif request.method=='PATCH':
         try:
             f = request.get_json()
-            user_name, category_id = f['user_name'], f['category_id']
+            user_name, category_id = f['model_name'], f['category_no']
             views.gallery_info(model_id, user_name, category_id)
             return jsonify({"success" : True})
         except:
